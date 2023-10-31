39d8a8920b75d85a344197f17109b683632059a5
Aimar Adhitya <aimaradhitya@gmail.com>
2023-10-31 09:27:40 +0800

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "aimardcr.co.id.bypass_dana"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
