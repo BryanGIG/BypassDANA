f2e58deada62e032284148862bdeaf029821b08e
BryanGIG <contacts.bryangig@gmail.com>
2024-07-11 17:11:12 +0700

    Bump v2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.bryangig.bypassdana"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.5.0"
+        versionCode = 260
+        versionName = "2.6.0"
     }
 
     buildTypes {
